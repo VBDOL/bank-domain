6b9dc614fe1c5c0443549d35539996bb185ae8b8
VBDOL. DEV <155402702+VBDOL@users.noreply.github.com>
2025-05-20 06:16:36 -0300

    Update ContaCorrente.java
    Taxa de manutenção

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,6 +1,13 @@
 public class ContaCorrente extends Conta {
+    private static final double TAXA_MANUTENCAO = 12.0;
+
     public ContaCorrente(Cliente cliente) {
         super(cliente);
+    }
+
+    @Override
+    public void aplicarTaxas() {
+        saldo -= TAXA_MANUTENCAO;
     }
 
     @Override
