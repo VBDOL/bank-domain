f2dd6b2652a1b5d8f7a9d49e4a972e6094362dba
VBDOL. DEV <155402702+VBDOL@users.noreply.github.com>
2025-05-20 06:16:58 -0300

    Update ContaPoupanca.java
    Juros sobre saldo

--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -1,6 +1,17 @@
 public class ContaPoupanca extends Conta {
+    private static final double TAXA_JUROS = 0.005; // 0.5% ao mês
+
     public ContaPoupanca(Cliente cliente) {
         super(cliente);
+    }
+
+    public void aplicarJuros() {
+        saldo += saldo * TAXA_JUROS;
+    }
+
+    @Override
+    public void aplicarTaxas() {
+        aplicarJuros(); // Em vez de taxa, aplica juros
     }
 
     @Override
