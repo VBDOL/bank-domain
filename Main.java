e3b92b194e2570c1b2ed5cf5d5b6c0b39084417e
VBDOL. DEV <155402702+VBDOL@users.noreply.github.com>
2025-05-20 06:18:03 -0300

    Update Main.java
    Teste do sistema atualizado

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Banco meuBanco = new Banco("Banco Digital");
 
-        Cliente cliente1 = new Cliente("Alice");
-        Cliente cliente2 = new Cliente("Bob");
+        Cliente cliente1 = new Cliente("Alice", "1234");
+        Cliente cliente2 = new Cliente("Bob", "abcd");
 
         Conta cc = new ContaCorrente(cliente1);
         Conta poupanca = new ContaPoupanca(cliente2);
@@ -11,8 +11,17 @@ public class Main {
         meuBanco.adicionarConta(cc);
         meuBanco.adicionarConta(poupanca);
 
-        cc.depositar(1000);
-        cc.transferir(300, poupanca);
+        // Simulando autenticação
+        if (cliente1.autenticar("1234")) {
+            cc.depositar(1000);
+            cc.transferir(300, poupanca);
+        } else {
+            System.out.println("Autenticação falhou!");
+        }
+
+        // Aplicando taxas/juros
+        cc.aplicarTaxas();
+        ((ContaPoupanca) poupanca).aplicarJuros();
 
         meuBanco.listarContas();
     }
