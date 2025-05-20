03334ed94c1a3860690dbcf1b664eefa416eaeba
VBDOL. DEV <155402702+VBDOL@users.noreply.github.com>
2025-05-20 06:17:18 -0300

    Update Cliente.java
    Autenticação básica

--- Cliente.java
+++ Cliente.java
@@ -1,11 +1,17 @@
 public class Cliente {
     private String nome;
+    private String senha;
 
-    public Cliente(String nome) {
+    public Cliente(String nome, String senha) {
         this.nome = nome;
+        this.senha = senha;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
     }
 }
